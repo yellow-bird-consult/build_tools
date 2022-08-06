fbfcb868bd4a8a2f0a65b7ed2000bd5f0aba68b2
maxwell flitton <maxwellflitton@maxwells-MacBook-Pro.local>
2022-08-07 00:23:02 +0100

    updating routing

--- modules/ingress.sh
+++ modules/ingress.sh
@@ -3,14 +3,14 @@
 # do the routing for incoming traffic
 if [ $1 = "db" ]
 then
-    source ~/test_yb_tools/database.sh
+    source ~/yb_tools/database.sh
 elif [ $1 = "build" ]
 then
-    ~/test_yb_tools/./build_tool
+    ~/yb_tools/./build_tool
 elif [ $1 = "test" ]
 then
-    ~/test_yb_tools/testing.sh
+    ~/yb_tools/testing.sh
 elif [ $1 = "run" ]
 then
-    ~/test_yb_tools/run_dev_server.sh
+    ~/yb_tools/run_dev_server.sh
 fi
