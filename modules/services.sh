375e05a3a36b388a9a875eb2b54d9e29ae5c50aa
maxwell flitton <maxwellflitton@maxwells-MacBook-Pro.local>
2022-08-11 00:15:32 +0100

    adding pull out build

--- modules/services.sh
+++ modules/services.sh
@@ -1,21 +1,38 @@
 #!/usr/bin/env bash
 
-
-if [ ! -f ./Cargo.toml ]
-then
-    echo "not in a root of a Rust server"
-    exit 0
-fi
-
 if [ ! -d ./services ]
 then
     mkdir services
 fi
 
+# work out the platform this is running on
+if [ "$(uname)" = "Darwin" ] && [ "$(uname -m)" = "arm64" ]
+then
+    echo "Apple M1 detected"
+    PLATFORM="M1"
+elif [ "$(uname)" = "Darwin" ] && [ "$(uname -m)" = "x86_64" ]
+then
+    echo "Apple Intel detected"
+    PLATFORM="AppleIntel"
+else
+    echo "Linux detected"
+    PLATFORM="Linux"
+fi
 
 if [ $2 = "add" ]
 then 
     git clone --branch $4 https://github.com/yellow-bird-consult/$3.git ./services/$3
+    cd ./services/$3
+    if [ $PLATFORM = "M1" ]
+    then
+        cp ./builds/arch_build ./Dockerfile
+    elif [ $PLATFORM = "Linux" ]
+    then
+        cp ./builds/server_build ./Dockerfile
+    elif [ $PLATFORM = "AppleIntel" ]
+    then
+        cp ./builds/server_build ./Dockerfile
+    fi
 fi
 
 if [ $2 = "wipe" ]
@@ -27,3 +44,4 @@ fi
 # Add a refresh 
 # Add a switch branch
 # add an ls
+# Add environment variables .env file in the build tools directory => GIT_OWNER=yellow-bird-consult
